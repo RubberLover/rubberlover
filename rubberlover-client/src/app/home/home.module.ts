f1a6fccfea40caef230ea6d6011114854c98d400
Darin Alleman <darin.alleman@gmail.com>
2022-08-26 14:52:10 -0400

    feat: :sparkles: add login toasts

--- rubberlover-client/src/app/home/home.module.ts
+++ rubberlover-client/src/app/home/home.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { ToastModule } from 'primeng/toast';
 import { TireTableComponent } from './tire-table/tire-table.component';
+import { MessageService } from 'primeng/api';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -11,7 +15,13 @@ import { TireTableComponent } from './tire-table/tire-table.component';
   ],
   imports: [
     CommonModule,
-    TableModule
+    TableModule,
+    DialogModule,
+    ToastModule,
+    BrowserAnimationsModule
+  ],
+  providers: [
+    MessageService
   ]
 })
 export class HomeModule { }
--- rubberlover-client/src/app/home/home/home.component.ts
+++ rubberlover-client/src/app/home/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { MessageService } from 'primeng/api';
+import { map, Observable, tap } from 'rxjs';
 import { UserService } from 'src/app/login/user.service';
 
 @Component({
@@ -10,8 +11,27 @@ import { UserService } from 'src/app/login/user.service';
 export class HomeComponent implements OnInit {
   currentUserName: Observable<string>;
 
-  constructor(private _userService: UserService) {
-    this.currentUserName = this._userService.currentUser$.pipe(map(user => user ? user.name : ""));
+  constructor(
+    private _userService: UserService,
+    private _toastService: MessageService
+    ) {
+    this.currentUserName = this._userService.currentUser$
+      .pipe(
+        tap(user => {
+          if (user) {
+            this._toastService.add(
+              {severity:'success',
+                life: 2000,
+              summary:'Logged in!'});
+          } else {
+            this._toastService.add(
+              {severity:'info',
+              life: 2000,
+              summary:'Logged out!'});
+          }
+        }),
+        map(user => user ? user.name : "")
+      );
    }
 
 
